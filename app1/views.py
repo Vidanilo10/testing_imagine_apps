50facbe2c630f4087187b0f732015774bae33485
Victor <vidanilo10@gmail.com>
2022-03-14 13:33:34 -0500

    Adding creation support for testing model.

--- app1/views.py
+++ app1/views.py
@@ -21,3 +21,13 @@ class TestingModelViewSet(
 
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
+    def perform_create(self, serializer):
+        serializer.save()
